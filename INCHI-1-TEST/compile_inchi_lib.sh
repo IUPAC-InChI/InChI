02f9276ec6cdc0823188616b22f244d6c891e9c5
Gianluca Sforna <giallu@gmail.com>
2024-07-30 20:57:52 +0000

    use cmake to compile current inchi library

--- INCHI-1-TEST/compile_inchi_lib.sh
+++ INCHI-1-TEST/compile_inchi_lib.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# generate libinchi.so.1 (symlink) and libinchi.so.dev
-
-(cd INCHI-1-SRC/INCHI_API/libinchi/gcc &&
-make -j LIB_DIR=../../../../INCHI-1-TEST VERSION=.dev &&
-make clean)
+# build library in inchi.dev directory
+cmake -B inchi.dev && cmake --build inchi.dev
 
 gcc_version=$(gcc --version | head -n 1)
 inchi_version=$(git describe --tags)
