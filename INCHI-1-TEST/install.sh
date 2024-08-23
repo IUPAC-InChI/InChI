4364da5b49a0d2589a8c2217fa841ffe2abfab55
Jan C. Brammer <jan.c.brammer@gmail.com>
2024-08-23 07:25:09 +0000

    Add `cmake` to install script

--- INCHI-1-TEST/install.sh
+++ INCHI-1-TEST/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && apt install -y python3-pip
+apt update && apt install -y python3-pip cmake
 python3 -m pip install --upgrade --break-system-packages pip
 python3 -m pip install --break-system-packages -e .[invariance-tests,development]
 # Make `python3` available as `python`.
