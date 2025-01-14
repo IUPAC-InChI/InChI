c3dcff6de2f1605dfb7a5dd7c40fa8878a36964e
Djordje Baljozovic <121813753+djb-rwth@users.noreply.github.com>
2025-01-14 16:51:38 +0100

    Version 1.07.2

--- INCHI-1-SRC/INCHI_API/demos/test_ixa/src/moreitil.h
+++ INCHI-1-SRC/INCHI_API/demos/test_ixa/src/moreitil.h
@@ -25,8 +25,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
- * The InChI library and programs are free software developed under the
+*
+* The InChI library and programs are free software developed under the
  * auspices of the International Union of Pure and Applied Chemistry (IUPAC).
  * Originally developed at NIST.
  * Modifications and additions by IUPAC and the InChI Trust.
@@ -36,7 +36,8 @@
  *
  * info@inchi-trust.org
  *
- */
+*/
+
 
 #ifndef _MOREUTIL_H_
 #define _MOREUTIL_H_
