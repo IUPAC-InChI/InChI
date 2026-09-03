fb7cb503835605baee088cc76ff4dc40da2032b8
Jan C. Brammer <jan.brammer@protonmail.com>
2026-09-03 06:23:54 +0000

    Replace headers

--- INCHI-1-SRC/INCHI_API/demos/inchi_main/src/aux2atom.h
+++ INCHI-1-SRC/INCHI_API/demos/inchi_main/src/aux2atom.h
@@ -1,42 +1,7 @@
 /*
- * International Chemical Identifier (InChI)
- * Version 1
- * Software version 1.07
- * April 30, 2024
- *
- * MIT License
- *
+ * SPDX-License-Identifier: MIT
  * Copyright (c) 2024 IUPAC and InChI Trust
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
-*
-* The InChI library and programs are free software developed under the
- * auspices of the International Union of Pure and Applied Chemistry (IUPAC).
- * Originally developed at NIST.
- * Modifications and additions by IUPAC and the InChI Trust.
- * Some portions of code were developed/changed by external contributors
- * (either contractor or volunteer) which are listed in the file
- * 'External-contributors' included in this distribution.
- *
- * info@inchi-trust.org
- *
-*/
+ */
 
 
 /*
@@ -1162,7 +1127,7 @@ int INChITo_Atom( INCHI_IOSTREAM *inp_molfile, MOL_COORD **szCoord,
                                  *  Set number of hydrogen atoms
                                  */
                                     {
-                                        /* djb-rwth: removing redundant variables/code */ 
+                                        /* djb-rwth: removing redundant variables/code */
                                         if (valence == ISOLATED_ATOM)
                                         {
                                             atom[a1].num_iso_H[0] = 0;
@@ -1522,7 +1487,7 @@ int INChITo_Atom( INCHI_IOSTREAM *inp_molfile, MOL_COORD **szCoord,
         }
 
 #if ( defined(TARGET_API_LIB) || defined(TARGET_EXE_USING_API) )
-        /* cleanup 
+        /* cleanup
         if (!*at)
         {
             if (atom)
@@ -2800,17 +2765,17 @@ int INChITo_Atom( INCHI_IOSTREAM *inp_molfile, MOL_COORD **szCoord,
 #ifdef TARGET_EXE_USING_API
 
 /**********************************************************************************/
-int INChIToInchi_Input( INCHI_IOSTREAM *inp_molfile, 
-                        inchi_InputEx *orig_at_data, 
+int INChIToInchi_Input( INCHI_IOSTREAM *inp_molfile,
+                        inchi_InputEx *orig_at_data,
                         int bMergeAllInputStructures,
-                        int bDoNotAddH, 
-                        int vABParityUnknown, 
+                        int bDoNotAddH,
+                        int vABParityUnknown,
                         INPUT_TYPE nInputType,
-                        char *pSdfLabel, 
-                        char *pSdfValue, 
-                        long *lSdfId, 
+                        char *pSdfLabel,
+                        char *pSdfValue,
+                        long *lSdfId,
                         INCHI_MODE *pInpAtomFlags,
-                        int *err, 
+                        int *err,
                         char *pStrErr )
 {
     /* inp_ATOM       *at = NULL; */
--- INCHI-1-SRC/INCHI_BASE/src/ikey_dll.c
+++ INCHI-1-SRC/INCHI_BASE/src/ikey_dll.c
@@ -1,51 +1,9 @@
 /*
- * International Chemical Identifier (InChI)
- * Version 1
- * Software version 1.07
- * April 30, 2024
- *
- * MIT License
- *
+ * SPDX-License-Identifier: MIT
  * Copyright (c) 2024 IUPAC and InChI Trust
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
-*
-* The InChI library and programs are free software developed under the
- * auspices of the International Union of Pure and Applied Chemistry (IUPAC).
- * Originally developed at NIST.
- * Modifications and additions by IUPAC and the InChI Trust.
- * Some portions of code were developed/changed by external contributors
- * (either contractor or volunteer) which are listed in the file
- * 'External-contributors' included in this distribution.
- *
- * info@inchi-trust.org
- *
-*/
+ */
 
-
-/*
-    InChIKey: calculation of hash for InChI string
-
-    Uses truncated SHA-256 function.
-    SHA-256 implementation: Copyright (C) Brainspark B.V.,
-                            see files sha2.c, sha2.h.
-*/
+/* InChIKey: calculation of hash for InChI string. Uses truncated SHA-256 function. */
 
 #ifdef _MSC_VER
 #if _MSC_VER > 1000
@@ -326,7 +284,7 @@ int INCHI_DECL GetINCHIKeyFromINCHI( const char* szINCHISource,
             goto fin;
         }
 
-        memcpy(sproto, str + pos_slash1 + ncp + 1, lenproto * sizeof(str[0]));   
+        memcpy(sproto, str + pos_slash1 + ncp + 1, lenproto * sizeof(str[0]));
         sproto[lenproto] = '\0';
 
         nprotons = strtol( sproto + 2, NULL, 10 );
